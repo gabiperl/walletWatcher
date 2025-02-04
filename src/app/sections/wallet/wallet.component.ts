0ef63fd1ddda514b46106dc9c7b2dab706030ebc
gabi <gabriel.perl@gmail.com>
2025-02-04 14:22:04 +0100

    Las monedas aparecen en Wallet tras darle al botón de favoritos.

--- src/app/sections/wallet/wallet.component.ts
+++ src/app/sections/wallet/wallet.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AssetQuantityComponent } from "./asset-quantity/asset-quantity.component";
 import { AssetActionsComponent } from "./asset-actions/asset-actions.component";
+import { WalletService } from '../../wallet.service';
 
 @Component({
   selector: 'app-wallet',
@@ -10,4 +11,8 @@ import { AssetActionsComponent } from "./asset-actions/asset-actions.component";
 })
 export class WalletComponent {
 
+  constructor(private wallet:WalletService) { }
+  getWalletAssets() {
+    return this.wallet.getWalletAssets();
+  }
 }
---
+++ src/app/wallet.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WalletService {
+
+ private wallet:any[] = []
+ 
+  constructor() { }
+
+  addToWallet(coin:any){
+    if (!this.isAlreadyInWallet(coin))
+        // this.wallet.push(coin)}
+        this.wallet.push({
+          ...coin,
+          quantity: 0
+        })}
+
+  getWalletAssets()
+  {return this.wallet}
+
+  isAlreadyInWallet(coin:any)
+  {return this.wallet.find(c => c.id == coin.id)}
+
+  removeFromWallet(coin:any){
+    let position = this.wallet.findIndex(c => c.id == coin.id)
+    this.wallet.splice(position, 1);
+  }
+}
